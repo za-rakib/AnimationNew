1d9c9c503652cfa9f31fdc32f42f56c113d6c978
Rakib <rakib@sayburgh.com>
2024-02-13 15:44:55 +0600

    circle to square

--- App.js
+++ App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import GestureHandler from './Screen/GestureHandler';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import Flag from './Screen/Flag';
+import GestureHandler2 from './Screen/GestureHandler2';
 export default function App() {
   return (
     <GestureHandlerRootView style={{flex: 1}}>
       {/* content */}
-      <GestureHandler />
+      <GestureHandler2 />
       {/* <Flag/> */}
     </GestureHandlerRootView>
   );
--- Screen/GestureHandler2.js
+++ Screen/GestureHandler2.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/self-closing-comp */
 import {View, Text, StyleSheet} from 'react-native';
 import React from 'react';
 import {Gesture, GestureDetector} from 'react-native-gesture-handler';
@@ -5,56 +6,63 @@ import Animated, {
   useAnimatedStyle,
   useSharedValue,
   withSpring,
+  withTiming,
 } from 'react-native-reanimated';
 
-const GestureHandler = () => {
+const GestureHandler2 = () => {
   const isPressed = useSharedValue(false);
   const offset = useSharedValue({x: 0, y: 0});
   const start = useSharedValue({x: 0, y: 0});
-
   const animatedStyles = useAnimatedStyle(() => {
     return {
       transform: [
         {translateX: offset.value.x},
         {translateY: offset.value.y},
-        {scale: withSpring(isPressed.value ? 1.3 : 1)},
+        {
+          scale: withTiming(isPressed.value ? 1.3 : 1, {duration: 1000}),
+        },
       ],
-      backgroundColor: isPressed.value ? 'yellow' : 'blue',
+      backgroundColor: withTiming(isPressed.value ? 'green' : 'red', {
+        duration: 1000,
+      }),
+      borderRadius: withTiming(isPressed.value ? 50 : 0, {duration: 1000}),
     };
   });
+  // console.log({isPressed: offset.value});
   const gesture = Gesture.Pan()
     .onBegin(() => {
       isPressed.value = true;
     })
     .onUpdate(e => {
-      console.log({e});
+      // console.log({e});
+      // offset.value.x = e.translationX + start.value.x;
+      // offset.value.y = e.translationY + start.value.y;
       offset.value = {
         x: e.translationX + start.value.x,
         y: e.translationY + start.value.y,
       };
     })
     .onEnd(() => {
-      start.value = {
-        x: offset.value.x,
-        y: offset.value.y,
-      };
+      start.value.x = offset.value.x;
+      start.value.y = offset.value.y;
+      // isPressed.value = false;
     })
     .onFinalize(() => {
       isPressed.value = false;
     });
   return (
     <GestureDetector gesture={gesture}>
-      <Animated.View style={[styles.ball, animatedStyles]} />
+      <Animated.View style={[styles.square, animatedStyles]} />
     </GestureDetector>
   );
 };
 const styles = StyleSheet.create({
-  ball: {
+  square: {
     width: 100,
     height: 100,
-    borderRadius: 100,
-    backgroundColor: 'blue',
+    backgroundColor: 'red',
     alignSelf: 'center',
+    marginTop: 20,
   },
 });
-export default GestureHandler;
+export default GestureHandler2;
